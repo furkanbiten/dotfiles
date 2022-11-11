343183a62b2d245ea253c5bd5f64513d5596e257
Ali Biten <abiten@Alis-MacBook-Pro.local>
2022-11-11 10:06:19 +0100

    fixed setting up server

--- setup_server.sh
+++ setup_server.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.3/install.sh | bash
-nvm install node
+export NVM_DIR=$HOME/.nvm;
+. $NVM_DIR/nvm.sh;
+nvm install node;
 
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+. "$HOME/.cargo/env"
 cargo install ripgrep
 
 wget https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.tar.gz
@@ -10,7 +14,7 @@ tar xf nvim-linux64.tar.gz
 echo "alias nvim="~/nvim-linux64/bin/nvim"" >> ~/.bashrc
 
 git clone https://github.com/furkanbiten/dotfiles
-cp -r .config/nvim ~/
+cp -r dotfiles/.config/nvim ~/.config/
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
