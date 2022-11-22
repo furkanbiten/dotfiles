8e49e696af6749288b0b9f42195d510a16d714a1
Ali Biten <abiten@Alis-MacBook-Pro.local>
2022-11-22 17:13:24 +0100

    adding lua installation

--- setup_server.sh
+++ setup_server.sh
@@ -19,3 +19,8 @@ cp -r dotfiles/.config/nvim ~/.config/
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+curl -R -O http://www.lua.org/ftp/lua-5.4.4.tar.gz
+tar zxf lua-5.4.4.tar.gz
+cd lua-5.4.4
+make all test
+echo "alias lua="~/lua-5.4.4/src/lua""
