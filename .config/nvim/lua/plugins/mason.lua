e89927fb5247902ab9d344b98b62a793a39d47c7
furkanbiten <furkanbiten@gmail.com>
2023-03-06 18:43:16 +0100

    Update mason.lua

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -1,7 +1,7 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-	ensure_installed = { "sumneko_lua", "pyright", "vimls", "yamlls", "jsonls" },
+	ensure_installed = { "pyright", "vimls", "yamlls", "jsonls" },
 })
 require("mason-null-ls").setup({
-	ensure_installed = { "stylua", "jq" },
+	ensure_installed = {},
 })
