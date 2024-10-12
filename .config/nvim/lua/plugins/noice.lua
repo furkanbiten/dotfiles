049e063aebf735d429319fcdae39dad89b5178ba
Ali Furkan Biten <furkanbiten@gmail.com>
2024-10-12 15:37:41 +0200

    fix: remove noice (waiting for a fix on cursor flickering)

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,17 +1,17 @@
--- return {}
+return {}
 -- Still a bit buggy but for sure later i will give it a try
-return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = {
-        -- add any options here
-    },
-    dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-        "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
-    }
-}
+-- return {
+--     "folke/noice.nvim",
+--     event = "VeryLazy",
+--     opts = {
+--         -- add any options here
+--     },
+--     dependencies = {
+--         -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+--         "MunifTanjim/nui.nvim",
+--         -- OPTIONAL:
+--         --   `nvim-notify` is only needed, if you want to use the notification view.
+--         --   If not available, we use `mini` as the fallback
+--         "rcarriga/nvim-notify",
+--     }
+-- }
