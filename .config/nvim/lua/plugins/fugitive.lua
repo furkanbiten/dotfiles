64d9313740d3775d2e4407c94c69180b4649eca0
Ali Furkan Biten <furkanbiten@gmail.com>
2024-10-12 14:39:36 +0200

    chore: clean-up

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -25,7 +25,7 @@ return {
 
                 -- NOTE: It allows me to easily set the branch i am pushing and any tracking
                 -- needed if i did not set the branch up correctly
-                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+                vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
             end,
         })
 
