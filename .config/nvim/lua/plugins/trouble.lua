f1e5d1cf1835ba0821bec031f3276af9e96a458e
Ali Furkan Biten <furkanbiten@gmail.com>
2024-10-12 04:05:02 +0200

    feat: new mappings for trouble

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,23 +1,37 @@
 return {
-    {
-        "folke/trouble.nvim",
-        event = "VeryLazy",
-        config = function()
-            require("trouble").setup({
-                icons = false,
-            })
-
-            vim.keymap.set("n", "<leader>xx", function()
-                require("trouble").toggle()
-            end)
-
-            vim.keymap.set("n", "[t", function()
-                require("trouble").next({ skip_groups = true, jump = true });
-            end)
-
-            vim.keymap.set("n", "]t", function()
-                require("trouble").previous({ skip_groups = true, jump = true });
-            end)
-        end
+    "folke/trouble.nvim",
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>xx",
+            "<cmd>Trouble diagnostics toggle<cr>",
+            desc = "Diagnostics (Trouble)",
+        },
+        {
+            "<leader>xX",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+            desc = "Buffer Diagnostics (Trouble)",
+        },
+        {
+            "<leader>cs",
+            "<cmd>Trouble symbols toggle focus=false<cr>",
+            desc = "Symbols (Trouble)",
+        },
+        {
+            "<leader>cl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+            desc = "LSP Definitions / references / ... (Trouble)",
+        },
+        {
+            "<leader>xL",
+            "<cmd>Trouble loclist toggle<cr>",
+            desc = "Location List (Trouble)",
+        },
+        {
+            "<leader>xQ",
+            "<cmd>Trouble qflist toggle<cr>",
+            desc = "Quickfix List (Trouble)",
+        },
     },
 }
