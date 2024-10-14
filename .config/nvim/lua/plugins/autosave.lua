7bb651e6dd57bd87fdeea39d4a1c0712ee564ec7
Ali Furkan Biten <furkanbiten@gmail.com>
2024-10-14 21:34:28 +0200

    fix: change auto-save to maintained fork

--- .config/nvim/lua/plugins/autosave.lua
+++ .config/nvim/lua/plugins/autosave.lua
@@ -1,5 +1,5 @@
 return {
-    "Pocco81/auto-save.nvim",
+    "okuuva/auto-save.nvim",
     config = function()
         require("auto-save").setup({
             debounce_delay = 2500,     -- saves the file at most every `debounce_delay` milliseconds
